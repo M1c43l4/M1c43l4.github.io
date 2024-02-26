c0bf5a590affab6d35340712fb540763e7df1dfe
M1c43l4 <161302514+M1c43l4@users.noreply.github.com>
2024-02-26 03:18:11 -0600

    Add files via upload
    initial commit

---
+++ Paginaweb.js
@@ -0,0 +1,173 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+    <title>Pruebas de usabilidad</title>
+    <link rel="stylesheet" href="index.css">
+    <!-- Enlaza Material Icons -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+
+
+<body>
+
+    <header>
+        <img src="amigotipo1.png" class="imagotipo">
+
+        <div class="container">
+            
+
+            <p class="logo"> Crisálida </p>
+            <h2> Pruebas de usabilidad</h2>
+            
+            <button><a href="Ingreso.html"> INGRESO </a></button>
+            <button><a href="Registro.html"> REGISTRO </a></button>
+        </div>
+    </header>
+
+    <section id="menu">
+       <div>
+        <nav>
+        <div class="buscar">
+            <input id="searchbar" onkeyup="Buscar()" type="text"
+            name="Buscar" placeholder="Buscar">
+        </div>
+
+
+        <ul class="nav"> 
+		
+            <li> <a href="Nosotros.html"> Nosotros </a></li>
+           
+            <li> <a href="#"></a> Proyectos
+                 <ul>
+                     <li> <a href="Historia.html"> Historia </a></li>
+                     <li> <a href="https://octopus.mx/blog/"> Blog </a></li>
+                 </ul>
+                     </li>
+       
+            <li> <a href="#"></a> Servicios 
+                 <ul>
+                     <li> <a href="Usabilidad.html">  Usabilidad </a></li>
+                     <li> <a href="Accesibilidad.html"> Accesibilidad </a></li>
+                </ul>
+                     </li>
+                
+           <li> <a href="#"></a> Cotización 
+                <ul>
+                    <li> <a href="Registro.html">Personal</a></li>
+                    <li> <a href="Registro.html">Empresarial</a></li>
+               </ul> 
+                    </li>
+       
+       </ul>
+       </nav>
+    </div>
+    </section>
+    
+       <section id="hero">
+        <div class="container">
+        
+
+                <div class="carta">
+                    <h3>¿Qué es usabilidad?</h3>
+                    <p>La usabilidad se refiere a la facilidad con que las personas pueden utilizar una interfaz o producto para lograr una tarea específica. <br>  Esta se cumple cuando el diseño es intuitivo, eficiente, satisfactorio y accesible para todo tipo de usuarios.</p>
+                    <div class="Botones">
+                        <button><a href="Usa.html"> +Info</a></button>
+                </div>
+                </div>
+
+                <div class="carta">
+                    <h3>¿Qué es una prueba de usabilidad?</h3>
+                    <p>Las pruebas de usabilidad son una técnica de evaluación en la que los usuarios interactúan con un producto, sistema o sitio web <br> y se observa su comportamiento. </p>
+                    <button> <a href="prueb.html"> +Info   </a></button>
+                </div>
+                
+
+                <div class="carta">
+                    <h3>Tipos de pruebas de usabilidad</h3>
+                    <p>Existen 3, veamos cuáles son</p>
+                    <button> <a href="tipos.html"> +Info </a></button>
+                </div>  
+                    </div>
+                       
+                    </section>
+
+
+    <section id="Acercade-nosotros">
+        <div class="container">
+            <div class="img-container"></div>
+            <div class="texto">
+
+                <h1> Sobre <span class="color-acento">Nosotros</span></h1>
+                <p>Diseñamos productos y experiencias digitales innovadoras mediante la integración de diseño y tecnología
+
+                    Nuestro enfoque se centra en entender y exceder las expectativas de nuestros clientes, ofreciendo experiencias digitales que no solo capturan la esencia de sus marcas sino que también mejoran significativamente la interacción y satisfacción del usuario.</p>
+                
+                 </div>
+            </div>
+            <button> <a href="Nosotros.html"> Conoce más </a></button>
+        </div>
+        
+    </section>
+
+    
+
+    <section id="nuestros-programas">
+        <div class="container">
+            <div class="img-container">
+                <img src="Nosotros.jpg" class="fakeimage">
+                <div class="texto">
+             <h3>Ventajas de un plan de <br>
+                pruebas de usabilidad</h3>
+            <p> Los beneficios de un plan de pruebas de usabilidad son significativos para ti y para tu empresa. <br>
+                Incorporar las pruebas de usuario a tus operaciones es siempre un paso fundamental. Un plan de <br>
+                pruebas bien estructurado mejora la aleatoriedad de las ideas, proporcionando claras ventajas. <br>
+                Sin una estrategia, corres el riesgo de malgastar tu tiempo y recursos en actividades que pueden <br>
+                no proporcionar los resultados deseados.</p>
+        
+                </div>  
+            </div>
+      </div>
+    </section>
+
+    <section id="caracteristicas">
+        <div class="container">
+            
+             <h1> Nuestros departamentos </h1>
+         <p>Nuestros departamentos, cada uno compuesto por expertos altamente cualificados, <br>
+            trabajan en armonía para brindar a nuestros clientes servicios excepcionales y orientados a resultados <br>
+            Permítenos guiarte a través de nuestra estructura organizativa</p>
+           <p> <span class="color-acento">En nuestro equipo de desarrollo de software, los ingenieros altamente competentes trabajan incansablemente <br>
+           para crear aplicaciones innovadoras y sistemas informáticos robustos que se adaptan perfectamente a las necesidades <br> 
+           específicas de nuestros clientes. Ya sea en el frente o en el backend, nuestros desarrolladores son expertos en garantizar <br>
+           la estabilidad, seguridad y eficiencia de cada aplicación que producimos</span></p>
+        </div>
+    </section>
+
+    <section id="final">
+        <div class="container">
+            <div class="nav">
+                
+                <button><a href="Paginaweb.html">Inicio</a></button>
+        <button> <a href="Registro.html">Contacto</a></button>
+        <button><a href="https://www.youtube.com/watch?v=hl08IUf_kIA">Test</a></button>
+         </div>
+                </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <div class="img-container">
+
+                <i class="material-icons">home</i> <!-- Icono de una casa de Material Icons -->
+                <i class="material-icons">email</i> <!-- Icono de un sobre de Material Icons -->
+               
+            <p>&copy; Crisálida</p>
+        </div>
+    </div>
+    </footer>
+
+</body>
+
+
+
+</html>
